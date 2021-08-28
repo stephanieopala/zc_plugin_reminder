9abcb43a99567b639a0f6ce4a90ad32d998d42a0
jutivia <rukkylaw.tl@gmail.com>
2021-08-28 22:56:35 +0100

    [Display Plugin Name]: Closes #108 Adding Plugin Name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import DisplayName from './Name/DisplayName'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ZC-PLUGIN
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DisplayName/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/Name/DisplayName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayName = () => {
+    return (
+        <div className='name' >
+            <h1>Company Reminder PlugIn </h1>
+        </div>
+    )
+}
+
+export default DisplayName
